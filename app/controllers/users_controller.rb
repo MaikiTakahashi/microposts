286e7803cbdbd1acfcd2f68931af356b96e53f7d
Maiki Takahashi <m1223takahashi@gmail.com>
2017-02-02 11:51:09 +0000

    プロフィールの更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
@@ -18,9 +22,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def correct_user
+    redirect_to(root_url) unless @user == current_user
+  end
+
 end
